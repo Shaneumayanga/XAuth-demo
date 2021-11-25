787275c5fb97f017cbdf372ea1558df6bbc177ba
Shane Umayanga <shaneumayanga445@gmail.com>
2021-11-26 03:02:49 +0530

    flow change

--- app.js
+++ app.js
@@ -3,29 +3,39 @@ const axios = require('axios');
 const app = express();
 const PORT = 8080;
 
-const CLIENT_ID = "8b7367b2-8295-40f7-af2a-606ddddabcca"
-const CLIENT_SECRET = "nCN8jOqZ0S6E3tkehv5g"
+const CLIENT_ID = "710e010d-4f51-4896-a8b6-5edfbb038dd1"
+const CLIENT_SECRET = "tvx5qjILw4WQ23zB8MAU"
 
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"))
 
 
-app.get("/login", (req,res)=>{
+app.get("/login", (req, res) => {
     res.redirect(`https://xauth.shaneumayanga.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_url=http://localhost:8080/callback&response_type=code`)
 });
 
-app.get("/callback",  (req, res) => {
+app.get("/callback", async (req, res) => {
     const code = req.query.code;
-    axios({
-        method:'post',
-        url:`https://xauth.shaneumayanga.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`,
-        Headers:{
+    const tokenres = await axios({
+        method: 'post',
+        url: `https://xauth.shaneumayanga.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`,
+        Headers: {
             accept: 'application/json'
         }
-    }).then((response)=>{
-        const access_token = response.data.accessToken;
-        res.redirect(`/hello.html?access_token=${access_token}`)
+    });
+    const access_token = tokenres.data.accessToken;
 
-    })
+    const result = await axios({
+        method: 'post',
+        url: `https://xauth.shaneumayanga.com/login/oauth/user`,
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${access_token}`
+        }
+    });
+    console.log(result.data)
+    res.redirect(`/hello.html?name=${result.data.name}`)
+
+
 });
 
 
